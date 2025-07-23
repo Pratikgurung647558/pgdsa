a3d8f585625a8db6de29537bcf37a4bc2af03ed7
Pratikgurung647558 <gurungpratik678@gmail.com>
2025-07-24 00:07:17 +0545

    linkedlistprefinal

--- linkedlst.py
+++ linkedlst.py
@@ -27,13 +27,94 @@ class LinkedList:
         while itr:
             print(itr.data, end=" --> ")
             itr = itr.next
-ll = LinkedList()         # head = None
-ll.insert_at_end(1)
-ll.insert_at_end(2)
-ll.insert_at_start(20)    # head -> 20 -> None
-ll.insert_at_start(10)    # head -> 10 -> 20 -> None
-ll.show()                 # prints: 10 --> 20 -->
+    
+    def datalist(self,data_list):
+        
+        for data in data_list:
+            self.insert_at_start(data)
+    
+    #for count of linked list
+    
+    def count(self):
+        count = 0
+        itr = self.head  # start from head
+        while itr:       # loop while itr is not None
+            count += 1
+            itr = itr.next
+        return count
+    
+    #delete at the respective indices
+    def deleteat(self,indices):
+        itr=self.head
+        if (indices==0):
+            self.head=self.head.next
+        elif(indices<0 or indices>self.count()):
+            print("invalid")
+        else:
+            count=0
+            while(count!=indices-1 and itr.next.next!= None):
+                count+=1
+                itr=itr.next
+            itr.next=itr.next.next
+            itr.next=None         #for last elemnt del
+        
+    #insert at the given indices
+    def insert_at_choice(self,indices,data):
+        if (indices==0):
+            self.insert_at_start(data)
+        elif(indices<0 or indices>self.count()):
+            print("invalid")
+        else:
+            count=0
+            itr=self.head
+            while(count!=indices-1):
+                count+=1
+                itr=itr.next
+            node=Node(data,itr.next)
+            itr.next=node
+    
+    #insert after values found
+    def insert_after_value(self,data_after,data_insert):
+        itr=self.head
+        while(itr is not None):
+            if(itr.data==data_after):
+                node=Node(data_insert,itr.next)
+                itr.next=node
+                break
+            itr=itr.next
 
+      #remove by value
+    def remove_by_value(self,data_to_delete):
+        # if list is empty
+        if self.head is None:
+            return
+
+        # if the node to delete is head node
+        if self.head.data == data_to_delete:
+            self.head = self.head.next
+            return
+
+        itr = self.head
+        while itr.next is not None:
+            if itr.next.data == data_to_delete:
+                itr.next = itr.next.next
+                return
+            itr = itr.next
+               
+        
+
+        
+
+ll = LinkedList()         # head = None
+ll.datalist(['a','b','c'])
+ll.show()                
+print("length is",ll.count())
+ll.deleteat(2)
+# ll.insert_at_choice(2,'aa')
+# ll.insert_at_choice(0,'cc')
+# ll.insert_after_value('cc','dd')
+# ll.remove_by_value('dd')
+ll.show()
 # head: Always points to the first node of the list. Initially None, updated to the newest node at each insertion.
 # data: The value stored in a node (e.g., 10, 20).
 # node: A container for data and next (link to the next node).
